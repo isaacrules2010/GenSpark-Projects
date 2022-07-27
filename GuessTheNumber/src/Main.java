de1d0b933b20c2973a8e8a477ce1475cea76bd84
Isaacrules2010 <williamcunningham32@gmail.com>
2022-07-27 10:46:43 -0500

    Finished Project2

--- GuessTheNumber/src/Main.java
+++ GuessTheNumber/src/Main.java
@@ -1,5 +1,34 @@
-public class Main {
-    public static void main(){
+import java.util.Random;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.println("Hello! What is your name?\n");
+        String userName = userInput.nextLine();
+        System.out.printf("Well, %s, I am thinking of a number between 1 and 20.\nTake a guess.\n", userName);
+        int num = random.nextInt(19) + 1;
+        int guess = userInput.nextInt();
+        int guessCount = 0;
+
+        while(guessCount < 6){
+            guessCount++;
+
+            if(guess > num){
+                System.out.println("Your guess is too high.\nTake a guess.\n");
+                guess = userInput.nextInt();
+            }
+            else if(guess < num){
+                System.out.println("Your guess is too low.\nTake a guess.\n");
+                guess = userInput.nextInt();
+            }
+            else {
+                System.out.printf("Nice job, %s! You guessed my number in %d guesses!", userName, guessCount);
+                return;
+            }
+        }
+        System.out.printf("Sorry. You only had 6 guesses. Better luck next time, %s!", userName);
     }
 }
