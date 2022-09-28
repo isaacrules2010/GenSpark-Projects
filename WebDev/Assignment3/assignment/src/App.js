42f226ba6ef9026031cf7ef1c8a40b46f6cb08eb
Isaacrules2010 <williamcunningham32@gmail.com>
2022-09-28 16:07:57 -0500

    Assignment3 - Carousel non functional, otherwise everything else is roughly identical

--- WebDev/Assignment3/assignment/src/App.js
+++ WebDev/Assignment3/assignment/src/App.js
@@ -1,11 +1,17 @@
-import 
 
+
+import Footer from './Components/Footer.js';
+import Form from './Components/Form.js';
 import Header from './Components/Header.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Form/>
+      <br></br>
+      <Footer/>
+      
     </div>
   );
 }
